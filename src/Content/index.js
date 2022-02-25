51611f1da39a4c4758094eb513ba2e78be1bc99b
nathias <n@th1.as>
2022-02-25 18:30:15 +0100

    Add infinite loading

--- src/Content/index.js
+++ src/Content/index.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import useInfiniteLoader from "react-use-infinite-loader";
 import styled from "styled-components";
 import useComics from "../hooks/useComics";
 import Card from "../Card";
@@ -13,27 +14,45 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
 `;
+
 const Content = ({ type }) => {
-  const [display, setDisplay] = useState([...Array(20)]);
+  const limit = 20;
+  const ref = useRef(null);
+  const [display, setDisplay] = useState([...Array(limit)]);
   const [popup, setPopup] = useState(() => ({ active: false }));
-  const fetchNext = true;
-  const comics = useComics(type, fetchNext);
+  const [fetchNext, setFetchNext] = useState(false);
+  const { comics } = useComics(type, limit, fetchNext, setFetchNext);
+  const loadMore = useCallback(() => {
+    if (!fetchNext?.[display.length]) setFetchNext({ [display.length]: true });
+    // eslint-disable-next-line
+  }, [display.length]);
+  const { loaderRef } = useInfiniteLoader({ loadMore, canLoadMore: true });
 
   useEffect(() => {
     if (!!comics[type]) setDisplay([...comics[type]]);
   }, [type, comics]);
 
-  useEffect(() => {
-    console.log(popup);
-  }, [popup]);
-
-  console.log(popup);
-
   return (
-    <Wrapper >
+    <Wrapper
+      onScroll={() => {
+        return null;
+      }}
+    >
       {!!popup.active && <Popup data={popup.data} setPopup={setPopup} />}
       {display.map((card, idx) => {
-        return <Card data={card} key={card?.id ?? idx} setPopup={setPopup} />;
+        const trigger = display.length - 11 === idx;
+
+        return (
+          <>
+            <Card
+              data={card}
+              key={card?.id + idx ?? idx}
+              setPopup={setPopup}
+              ref={ref}
+            />
+            {trigger && <div ref={loaderRef} />}
+          </>
+        );
       })}
     </Wrapper>
   );
--- src/hooks/useComics.js
+++ src/hooks/useComics.js
@@ -1,23 +1,36 @@
 import { useState, useEffect } from "react";
 const marvelKey = process.env.REACT_APP_MARVEL_PUBLIC_KEY;
 const filledArray = [...Array(20)];
+
 const comicsDefault = {
+  default: true,
   all: filledArray,
   magazine: filledArray,
   comic: filledArray,
   "digital%20comic": filledArray,
 };
+const comicsEmpty = {
+  all: [],
+  magazine: [],
+  comic: [],
+  "digital%20comic": [],
+};
 
-const useComics = (type, fetchNext) => {
+const useComics = (type, limit, fetchNext, setFetchNext) => {
   const [comics, setComics] = useState(() => comicsDefault);
   const [isLoading, setIsLoading] = useState(false);
   const [newComics, setNewComics] = useState([]);
-  const offset = comics?.[type]?.length;
+  const offset = !!comics?.default
+    ? comics?.[type]?.length - limit
+    : comics?.[type]?.length;
   const format = type !== "all" ? "format=" + type : "";
-  const url = `https://gateway.marvel.com:443/v1/public/comics?${format}&limit=20&offset=${offset}&apikey=${marvelKey}`;
+  const url = `https://gateway.marvel.com:443/v1/public/comics?${format}&limit=${limit}&offset=${offset}&apikey=${marvelKey}`;
 
   useEffect(() => {
     setIsLoading(true);
+    if (!comics.default && !fetchNext?.[offset] && !!comics[type]?.length)
+      return;
+    setFetchNext(false);
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
@@ -26,20 +39,21 @@ const useComics = (type, fetchNext) => {
           setIsLoading(false);
         }
       });
-  }, [type, url]);
+    // eslint-disable-next-line
+  }, [type, url, fetchNext, comics]);
 
   useEffect(() => {
     if (!isLoading) {
       const payload = [];
-      const oldData = comics ?? [];
-      newComics.forEach((comic) => {
-        if (!comics[type].includes(comic)) {
+      const oldData = !!comics?.default ? comicsEmpty : comics;
+      newComics?.forEach((comic) => {
+        if (!comics[type].some((oldComic) => oldComic?.id === comic.id)) {
           payload.push(comic);
         }
       });
       const update = {
         ...oldData,
-        [type]: payload,
+        [type]: [...oldData[type], ...payload],
       };
 
       setComics(update);
@@ -47,7 +61,7 @@ const useComics = (type, fetchNext) => {
     // eslint-disable-next-line
   }, [newComics, isLoading]);
 
-  return comics;
+  return { comics, isLoading };
 };
 
 export default useComics;
